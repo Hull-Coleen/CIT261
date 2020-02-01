09ec6a78cb226c3e98593c0669b3a1a4c1c0f001
Hull-Coleen <ch.hull@verizon.net>
2020-02-01 18:11:17 -0500

    javascript object
    made a couple of changes to show how each instance works better and made a few comments.

--- object_jss.js
+++ object_jss.js
@@ -1,14 +1,20 @@
 function Animal(type, mammal, size) {
+	// properties
 	this.type = type;
 	this.mammal = mammal;
 	this.size = size;
+	this.animalSpecs = function() { return this.type + " " + this.mammal; };
 
 }
+// added new property using prototype property
 Animal.prototype.habitat = "SouthWest United States";
+// added new fucntion using prototype property
 Animal.prototype.hab = function() {
 	return this.habitat;
+	
 }
 
+// gets input from user and create a new instance of Animal
 function createAnimal() {
 	var t = document.getElementById("animalType").value;
 	if ( t == ""){
@@ -27,13 +33,29 @@ function createAnimal() {
 	return a;
 
 }
-//var b = createAnimal();
-function showAnimal() {
-	var a = createAnimal();
+// new instance
+function createAnimalWithoutUserInput() {
+   var tiger = new Animal("tiger", "mammal", "large");
+   tiger.habitat = "Africa";
+return tiger;
+}
+// displays the information of the Animal Contructor
+function showAnimal(a) {
+	
 	a.animalInfo = function() {return this.type + " " + this.mammal; }; 
 	// use of properties
 	document.getElementById("showAnimal").innerHTML = a.type + "  " + a.mammal + "  " + a.size;
+	// use of constructor methods
 	document.getElementById("showAnimal2").innerHTML = a.animalInfo();
 	document.getElementById("showAnimal3").innerHTML = a.hab();
+	document.getElementById("showAnimal4").innerHTML = a.animalSpecs();
+	
 	
 }
+function showWhatDoesNotWork() {
+	var t = createAnimalWithoutUserInput();
+	//t.animalInfo();
+	return t;
+}
+
+
